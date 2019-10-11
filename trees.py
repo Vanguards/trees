8fe0d937da9c81b6ecd51a1f4394bd8ad5a56a3b
Vanguards <1094194524@qq.com>
2019-10-12 00:11:29 +0800

    Initial commit

---
+++ trees.py
@@ -0,0 +1,35 @@
+from math import log
+
+
+
+def calcShannonEnt(dataSet):
+    numEntries=len(dataSet)
+    labelCounts={}
+    for featVec in dataSet:
+        currentLabel=featVec[-1]
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel]=0
+        labelCounts[currentLabel]+=1
+    shannonEnt=0.0
+    for key in labelCounts:
+        prob=float(labelCounts[key])/numEntries
+        shannonEnt-=prob*log(prob,2)
+    return shannonEnt
+
+def createDataSet():
+    dataset=[[1,1,'maybe'],[1,1,'yes'],[1,0,'no'],[0,1,'no'],[0,1,'no']]
+    labels=['no surfacing','flippers']
+    return dataset,labels
+
+
+def splitDataSet(dataSet,axis,value):
+    retDataSet=[]
+    for featVec in dataSet:
+        if featVec[axis]==value:
+            reducedFeatVec=featVec[:axis]
+            reducedFeatVec.extend(featVec[axis+1:])
+            retDataSet.append(reducedFeatVec)
+    return retDataSet
+
+
+
