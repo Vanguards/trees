2149a2cc76581c273768e72695038bdeb3e7d396
Vanguards <1094194524@qq.com>
2019-10-12 16:36:20 +0800

    第三次提交

--- trees.py
+++ trees.py
@@ -1,4 +1,5 @@
 from math import log
+import operator
 
 
 
@@ -59,6 +60,13 @@ dataSet,label = createDataSet()
 print(chooseBestFeatureToSplit(dataSet))
 
 
-
+def majorityCnt(classList):
+    classCount={}
+    for vote in classList:
+        if vote not in classCount.keys():
+            classCount[vote]=0
+        classCount[vote]+=1
+    sortedClassCount=sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
 
 
